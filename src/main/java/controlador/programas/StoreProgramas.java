86f171cae90f2d427be4aaeec5596027f3bd3ee2
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-05-16 12:33:57 +0200

    Ordenado controlador programas

--- src/main/java/controlador/programas/StoreProgramas.java
+++ src/main/java/controlador/programas/StoreProgramas.java
@@ -1,45 +1,47 @@
 package controlador.programas;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import modelo.ModeloAlumno;
 import modelo.ModeloCurso;
 import modelo.ModeloPrograma;
 import modelo.Programa;
 
-
 /**
- * Servlet implementation class StoreProgramas
+ * Implementación del Servlet StoreProgramas.
+ * Este servlet se encarga de almacenar un programa en la base de datos.
  */
 @WebServlet("/StoreProgramas")
 public class StoreProgramas extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
-     * @see HttpServlet#HttpServlet()
+     * Constructor del servlet StoreProgramas.
      */
     public StoreProgramas() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Maneja las solicitudes HTTP GET.
+	 * 
+	 * @param request  Objeto HttpServletRequest que contiene la solicitud realizada por el cliente al servlet
+	 * @param response Objeto HttpServletResponse que contiene la respuesta que el servlet envía al cliente
+	 * @throws ServletException si se produce un error en la ejecución del servlet
+	 * @throws IOException      si se produce un error de entrada/salida
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//recibir los datos del programa
-		
+		// Recibir los datos del programa
 		String contenido = request.getParameter("contenido");
 		String criterioEvaluacion = request.getParameter("criterioEvaluacion");
 		int idCurso = Integer.parseInt(request.getParameter("idCurso"));
 		
-		//almacenar el programa en la bbdd
+		// Almacenar el programa en la base de datos
 		ModeloCurso modeloCurso = new ModeloCurso();
 		Programa programa = new Programa();
 		programa.setContenido(contenido);
@@ -48,25 +50,25 @@ public class StoreProgramas extends HttpServlet {
 		
 		ModeloPrograma modeloPrograma = new ModeloPrograma();
 		
+		// Enviar respuesta y decidir si insertar o no
 		if(idCurso == 0) {
 			response.sendRedirect("IndexProgramas?msg=insertError");
 		} else {
 			modeloPrograma.insert(programa);
 			response.sendRedirect("IndexProgramas?msg=insertOk");
 		}
-		
-		
-		
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * Maneja las solicitudes HTTP POST.
+	 * 
+	 * @param request  Objeto HttpServletRequest que contiene la solicitud realizada por el cliente al servlet
+	 * @param response Objeto HttpServletResponse que contiene la respuesta que el servlet envía al cliente
+	 * @throws ServletException si se produce un error en la ejecución del servlet
+	 * @throws IOException      si se produce un error de entrada/salida
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// Redirigir las solicitudes POST a doGet
 		doGet(request, response);
-		
-		
 	}
-
 }
